b35e14411d22cacc15ad2848f4751090a41c2e24
intflag <1598749808@qq.com>
2020-03-22 23:39:06 +0800

    2020-3-22打卡提交

---
+++ 215.数组中的第k个最大元素.java
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=java
+ *
+ * [215] 数组中的第K个最大元素
+ *
+ * https://leetcode-cn.com/problems/kth-largest-element-in-an-array/description/
+ *
+ * algorithms
+ * Medium (61.49%)
+ * Likes:    405
+ * Dislikes: 0
+ * Total Accepted:    94.1K
+ * Total Submissions: 152.5K
+ * Testcase Example:  '[3,2,1,5,6,4]\n2'
+ *
+ * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [3,2,1,5,6,4] 和 k = 2
+ * 输出: 5
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+ * 输出: 4
+ * 
+ * 说明: 
+ * 
+ * 你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+
+    /** 排序法 */
+    // public int findKthLargest(int[] nums, int k) {
+    //     Arrays.sort(nums);
+    //     return nums[nums.length - k];
+    // }
+
+    /**优先队列实现 */
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i : nums) {
+            pq.add(i);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    } 
+}
+// @lc code=end
+
