2bace9f85b30ab1075af5c8d9e76fd422c5c0a1f
intflag <1598749808@qq.com>
2020-04-03 23:30:08 +0800

    2020-4-3 AC

---
+++ java/677.键值映射.java
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=677 lang=java
+ *
+ * [677] 键值映射
+ *
+ * https://leetcode-cn.com/problems/map-sum-pairs/description/
+ *
+ * algorithms
+ * Medium (60.65%)
+ * Likes:    40
+ * Dislikes: 0
+ * Total Accepted:    5.5K
+ * Total Submissions: 9K
+ * Testcase Example:  '["MapSum", "insert", "sum", "insert", "sum"]\n' +
+  '[[], ["apple",3], ["ap"], ["app",2], ["ap"]]'
+ *
+ * 实现一个 MapSum 类里的两个方法，insert 和 sum。
+ * 
+ * 对于方法 insert，你将得到一对（字符串，整数）的键值对。字符串表示键，整数表示值。如果键已经存在，那么原来的键值对将被替代成新的键值对。
+ * 
+ * 对于方法 sum，你将得到一个表示前缀的字符串，你需要返回所有以该前缀开头的键的值的总和。
+ * 
+ * 示例 1:
+ * 
+ * 输入: insert("apple", 3), 输出: Null
+ * 输入: sum("ap"), 输出: 3
+ * 输入: insert("app", 2), 输出: Null
+ * 输入: sum("ap"), 输出: 5
+ * 
+ * 
+ */
+
+// @lc code=start
+class MapSum {
+
+    /** Initialize your data structure here. */
+    public MapSum() {
+
+    }
+    
+    public void insert(String key, int val) {
+
+    }
+    
+    public int sum(String prefix) {
+
+    }
+}
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
+// @lc code=end
+
