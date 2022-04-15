3a89f6fe5f3631db1925c77ef6da3a0fa1f173cf
liugx <liuguoxin@tianyancha.com>
2022-04-15 14:40:54 +0800

    test alg

--- java/541.反转字符串-ii.java
+++ java/541.反转字符串-ii.java
@@ -52,7 +52,7 @@
 class Solution {
     public String reverseStr(String s, int k) {
         char[] arr = s.toCharArray();
-        for (int i = 0; i <= arr.length; i += 2 * k) {
+        for (int i = 0; i < arr.length; i += 2 * k) {
             if (i + k <= arr.length) {
                 reverse(arr, i, i + k);
             } else {
